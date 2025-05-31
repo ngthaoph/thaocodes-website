b8147c76a7248b3287a1e2c28d3e5a844e2232b7
Thao Nguyen <ngthaoph@gmail.com>
2025-05-31 22:50:46 +1000

    update gap on arrow div

--- src/layouts/Next.jsx
+++ src/layouts/Next.jsx
@@ -5,9 +5,11 @@ import { FaLongArrowAltRight } from "react-icons/fa";
 function Next({ handleActive, active }) {
   return (
     <div>
-      <h1 className="font-bold text-3xl flex flex-row items-center">
-        See my works
-        <FaLongArrowAltRight onClick={handleActive} />
+      <h1 className="font-bold text-3xl flex flex-row items-center gap-3">
+        <div>See My Works</div>
+        <div>
+          <FaLongArrowAltRight onClick={handleActive} />
+        </div>
       </h1>
       <div className="flex max-md:justify-center">{active && <Project />}</div>
     </div>
